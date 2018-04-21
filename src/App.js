8a3fbe25e255f554ff7282b5a83ad713d543299e
Ryan Liu <ryan@Ryans-MacBook-Pro.local>
2018-04-21 12:38:54 +1000

    Added geolocation in App.js to test the retrieval of user's current location.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, { PureComponent as Component } from 'react';
 import PopOver from './playground/PopOver';
 
 class App extends Component {
+
+  componentDidMount = () => {
+    this.getLocation();
+  }
+
+  getLocation = () => {
+    const nav = navigator.geolocation;
+    nav.getCurrentPosition(position => {
+      console.log("lat: ", position.coords.latitude);
+      console.log("lon: ", position.coords.longitude);
+    })
+  }
+
   render() {
     return (
       <div>
