5d649b516d666a051c034ff86171e110db71f05e
Taryn <taryn@sugarpush.com.au>
2018-04-21 12:36:23 +1000

    working on swipe function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { PureComponent as Component } from 'react';
 import PopOver from './playground/PopOver';
+import Swipe from './friendships/Swipe'
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div>
         <h1>This is Home page</h1>
         <PopOver />
+        <Swipe />
       </div>
     )
   }
