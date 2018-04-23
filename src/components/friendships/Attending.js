de16dec0e749b8ca2d3b8aaffde95fd64ad46108
Taryn <taryn@sugarpush.com.au>
2018-04-24 09:34:47 +1000

    still working on rendering the correct buttons for attending users

--- src/components/friendships/Attending.js
+++ src/components/friendships/Attending.js
@@ -48,39 +48,50 @@ class Attending extends Component {
   }
 
   renderButton = (id) => {
-    for (let i = 0; i < this.state.friendships.length; i++) {
-      console.log("run loop");
+    if (this.state.friendships.length >= 1) {
 
-        if (this.state.friendships[i].friend_id === current_user.sub && this.state.friendships[i].user_id === id && this.state.friendships[i].active === false) {
-          console.log("patchClick");
-          return (<button onClick={() => this._handlePatchClick(id)}>Match</button>)
-        } else if (this.state.friendships[i].friend_id !== current_user.sub && this.state.friendships[i].user_id !== current_user.sub ) {
-          console.log("PostClick");
-          return (<button onClick={() => this._handlePostClick(id)}>Match</button>)
-        } else if (this.state.friendships[i].friend_id === current_user.sub && this.state.friendships[i].user_id === id && this.state.friendships[i].active === true) {
-          console.log("Match found - friend id is current user");
-          return (<p>EarBuddies!</p>)
-        } else if (this.state.friendships[i].friend_id === id && this.state.friendships[i].user_id === current_user.sub && this.state.friendships[i].active === true) {
-          console.log("Match found - user id is current user");
-          return (<p>EarBuddies!</p>)
-        } else if (this.state.friendships[i].friend_id === id && this.state.friendships[i].user_id === current_user.sub && this.state.friendships[i].active === false) {
-          console.log("Pending found");
-          return (<p>Already matched!</p>)
-        } else {
-          return (<button onClick={() => this._handlePostClick(id)}>Match</button>)
+      for (let i = 0; i < this.state.friendships.length; i++) {
+        console.log("run loop");
+
+          if (this.state.friendships[i].friend_id === current_user.sub && this.state.friendships[i].user_id === id && this.state.friendships[i].active === false) {
+            console.log("patchClick");
+            return (<button onClick={() => this._handlePatchClick(id)}>Match</button>)
+
+          } else if (this.state.friendships[i].friend_id === current_user.sub && this.state.friendships[i].user_id === id && this.state.friendships[i].active === true) {
+            console.log("Match found - friend id is current user");
+            return (<button onClick={() => this._handleDeleteClick(id)}>Buddies! Disconnect Buddy?</button>)
+
+          } else if (this.state.friendships[i].friend_id === id && this.state.friendships[i].user_id === current_user.sub && this.state.friendships[i].active === true) {
+            console.log("Match found - user id is current user");
+            return (<button onClick={() => this._handleDeleteClick(id)}>Buddies! Disconnect Buddy?</button>)
+
+          } else if (this.state.friendships[i].friend_id === id && this.state.friendships[i].user_id === current_user.sub && this.state.friendships[i].active === false) {
+            console.log("Pending found");
+            return (<button onClick={() => this._handleDeleteClick(id)}>Cancel Buddy Request?</button>)
+
+          } else if (current_user.sub === id){
+            return (<button onClick={() => this._handleCancelAttendingClick(id)}>Not attending anymore?</button>)
+            
+          } else {
+            return (<button onClick={() => this._handlePostClick(id)}>Match</button>)
+          }
         }
 
-      }
+    } else if (this.state.friendships.length < 1 && current_user.sub === id){
+      return (<button onClick={() => this._handleCancelAttendingClick(id)}>Not attending anymore?</button>)
+    } else {
+      return (<button onClick={() => this._handlePostClick(id)}>Match</button>)
+    }
     }
 
 
   _handlePatchClick(id){
     console.log("Friendship Found - Active Match made!");
     axios({
-      url: USERS_URL,
+      url: FRIENDSHIPS_URL,
       method: 'patch',
       headers: {
-        authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjQ1Mjc2MDMsInN1YiI6NywiZW1haWwiOiJ0YXJ5bkB0YXJ5bi5jb2RlcyIsImFkbWluIjp0cnVlfQ.1RBD0T6qoAe0fSL9hRhvPKdEvDgjPlnPvc9yi8FHTE8`
+        authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjQ2MTEyNTEsInN1YiI6MTQsImVtYWlsIjoidGFyeW5AdGFyeW4uY29kZXMiLCJhZG1pbiI6bnVsbH0.owYVjK7yMwdXPnbxblZ7ODyWxrXtwlwBW14KBF7Znpo`
       },
       data: {
       friendship: {
@@ -89,16 +100,17 @@ class Attending extends Component {
         active: true
         }
       }
-    }).then(res => this.setState({users: res.data}))
+    }).then(res => console.log(res))
   }
 
   _handlePostClick(id){
     console.log("No friendship found = Inactive Friendship Made!");
+    console.log(id);
     axios({
-      url: USERS_URL,
+      url: FRIENDSHIPS_URL,
       method: 'post',
       headers: {
-        authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjQ1Mjc2MDMsInN1YiI6NywiZW1haWwiOiJ0YXJ5bkB0YXJ5bi5jb2RlcyIsImFkbWluIjp0cnVlfQ.1RBD0T6qoAe0fSL9hRhvPKdEvDgjPlnPvc9yi8FHTE8`
+        authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjQ2MTEyNTEsInN1YiI6MTQsImVtYWlsIjoidGFyeW5AdGFyeW4uY29kZXMiLCJhZG1pbiI6bnVsbH0.owYVjK7yMwdXPnbxblZ7ODyWxrXtwlwBW14KBF7Znpo`
       },
       data: {
         friendship: {
@@ -107,7 +119,25 @@ class Attending extends Component {
           active: false
         }
       }
-    }).then(res => this.setState({users: res.data}))
+    }).then(res => console.log(res))
+  }
+
+  _handleDeleteClick(id){
+    console.log("No friendship found = Inactive Friendship Made!");
+    console.log(id);
+    axios({
+      url: USERS_URL,
+      method: 'delete',
+      headers: {
+        authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjQ2MTEyNTEsInN1YiI6MTQsImVtYWlsIjoidGFyeW5AdGFyeW4uY29kZXMiLCJhZG1pbiI6bnVsbH0.owYVjK7yMwdXPnbxblZ7ODyWxrXtwlwBW14KBF7Znpo`
+      },
+      data: {
+        friendship: {
+          friend_id: id,
+          active: false
+        }
+      }
+    }).then(res => console.log(res))
   }
 
 
