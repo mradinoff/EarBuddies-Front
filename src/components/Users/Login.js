e70f42c4a81db13c704fa795993c2178d3c837c4
Ryan Liu <ryan@Ryans-MacBook-Pro.local>
2018-04-23 10:18:12 +1000

    Added Login component.

---
+++ src/components/Users/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Login extends React.Component {
+    render() {
+        return (
+            <h1>This is Login page</h1>
+        );
+    }
+}
+
+export default Login;
