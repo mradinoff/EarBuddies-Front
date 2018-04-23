76a050f78a7352011be05a52ab782d42db17a37f
Joseph Ocampo <josephocampo@MacBook-Pro-6.local>
2018-04-23 13:45:04 +1000

    fixed login / logout

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -8,8 +8,8 @@ import FlatButton from 'material-ui/FlatButton';
 import Toggle from 'material-ui/Toggle';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
-
-
+import Login from '../users/Login';
+import SignUp from '../users/SignUp';
 import { Link } from 'react-router-dom'
 
 
@@ -29,14 +29,20 @@ class Nav extends Component {
     logged: true,
   };
 
+  _signOut() {
+    localStorage.setItem('jwtToken', '')
+  }
+
+
   render(){
     return (
       <header>
         <nav className="nav_bar">
           <Link to="/"><img src="" alt="logo"/></Link>
-          { window.localStorage.jwtToken ? <Link to="/signup">Logout</Link>:
+          { window.localStorage.jwtToken ? <Link onClick={this._signOut} to="/">Logout</Link> :
           <div>
             <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
           </div>
           }
         </nav>
