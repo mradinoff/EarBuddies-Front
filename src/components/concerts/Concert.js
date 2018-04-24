4b09d72e13dd0828482c4524f0ecf1ed40da7318
Joseph Ocampo <josephocampo@MacBook-Pro-6.local>
2018-04-24 12:37:59 +1000

    attending button in concerts

--- src/components/concerts/Concert.js
+++ src/components/concerts/Concert.js
@@ -13,37 +13,38 @@ class Concert extends Component{
       venue: [],
       concert: [this.props.location.state]
     }
-    // this.findVenue = this.findVenue.bind(this);
-    // console.log(this.state.concert[0])
+    this.findVenue = this.findVenue.bind(this);
+    console.log(this.state.concert[0])
   }
-  // componentDidMount = () => {
-  //   this.findVenue();
-  //   this.eventUserList();
-  // }
+  componentDidMount = () => {
+    this.findVenue();
+  }
 
-  // findVenue(state){
-  //
-  //     axios({
-  //       method:'GET',
-  //       url: `https://earbuddies1.herokuapp.com/venues/${this.state.concert[0].venue_id}.json`,
-  //       responseType: 'json',
-  //
-  //     }).then(function(v){
-  //       let venue = []
-  //       venue.push(v.data)
-  //       this.setState({venue})}.bind(this))
-  //
-  //       }
+  findVenue(state){
+
+      axios({
+        method:'GET',
+        url: `https://earbuddies1.herokuapp.com/venues/${this.state.concert[0].venue_id}.json`,
+        responseType: 'json',
+
+      }).then(function(v){
+        let venue = []
+        venue.push(v.data)
+        this.setState({venue})}.bind(this))
+
+        }
 
   deleteUserFromEvent = () => {
   console.log(current_user.sub);
+  console.log(this.props.location.state)
   console.log(parseInt(this.props.match.params.id));
     axios({
-      url: 'https://earbuddies1.herokuapp.com/events_users',
+      url: ``,
       method: 'delete',
-      data: {
+      data:{ users : {
         user_id: current_user.sub,
         event_id: parseInt(this.props.match.params.id)
+        }
       }
     }).then(response => {
      console.log(response)
@@ -76,20 +77,16 @@ class Concert extends Component{
     render (){
       return(
       <div>
-          {/* {this.state.concert.map(c => <li key={c.id}> {c.name}</li>)}
+          {this.state.concert.map(c => <li key={c.id}> {c.name}</li>)}
           {this.state.concert.map(c => <li key={c.id}> {c.description}</li>)}
           {this.state.concert.map(c => <li key={c.id}> {c.date}</li>)}
           {this.state.concert.map(c => <li key={c.id}> {c.genre}</li>)}
           {this.state.concert.map(c => <li key={c.id}> {c.ticket_url}</li>)}
           {this.state.concert.map(c => <img key={c.id} src={c.image} alt={c.name}/>)}
           {this.state.venue.map (v => <li key={v.id}> {v.name}</li>)}
-          {/* will go to venue page */} */}
-          <button onClick={this.addUserToEventList}>
-                  attending
-          </button>
-          <button onClick={this.deleteUserFromEvent}>
-                  not attending
-          </button>
+          {/* will go to venue page */}
+          <button onClick={this.addUserToEventList}>attending</button>
+          <button onClick={this.deleteUserFromEvent}>not attending</button>
       </div>
     )}
   }
