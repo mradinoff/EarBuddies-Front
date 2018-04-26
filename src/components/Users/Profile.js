6bd299779e609b07ee0d58b37b37e72e53fba962
Jonathan Casson <jonathancasson@Jonathans-MacBook-Pro-2.local>
2018-04-26 12:59:10 +1000

    pending profile changes

--- src/components/Users/Profile.js
+++ src/components/Users/Profile.js
@@ -1,7 +1,11 @@
 import React, { PureComponent as Component } from "react";
 import axios from "axios";
+import jwtDecoder from "jwt-decode";
 
-const USER_URL = 'https://earbuddies1.herokuapp.com/users/27.json';
+const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjQ3OTQxMTMsInN1YiI6MjcsImVtYWlsIjoidGFyeW5AdGFyeW4uY29kZXMiLCJhZG1pbiI6bnVsbH0.o3cmhVp2s3tPRz6Q1Km2Y2Vx8wm833kSPCv8NO0hhh0";
+const current_user = jwtDecoder(token);
+
+const USER_URL = `https://earbuddies1.herokuapp.com/users/${current_user.sub}.json`;
 
 
 class Profile extends Component {
